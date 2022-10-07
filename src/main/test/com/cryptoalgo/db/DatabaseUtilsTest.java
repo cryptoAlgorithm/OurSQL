ed02a374e3045618d8ab329c440edd97f4f9f03e
cryptoAlgorithm <cryptoalgro@gmail.com>
2022-10-07 13:02:24 +0800

    fix (tests): @Ignore connection test The test only works when localhost instance of PostgreSQL is running.

--- src/main/test/com/cryptoalgo/db/DatabaseUtilsTest.java
+++ src/main/test/com/cryptoalgo/db/DatabaseUtilsTest.java
@@ -2,12 +2,16 @@ package com.cryptoalgo.db;
 
 import com.cryptoalgo.oursql.model.db.Cluster;
 import com.cryptoalgo.oursql.model.db.DatabaseUtils;
+import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class DatabaseUtilsTest {
+    @Ignore("Only works when localhost instance of PostgreSQL is running")
     @Test
-    void testPostgreSQL() throws Exception {
-        DatabaseUtils
+    void testPostgreSQL() {
+        assertDoesNotThrow(() -> DatabaseUtils
             .getConnection(
                 new Cluster(
                     "postgresql",
@@ -18,6 +22,39 @@ class DatabaseUtilsTest {
                     ""
                 ),
                 null
-            );
+            )
+        );
+    }
+
+    @Test
+    void testGetConnectionURI() throws Exception {
+        // PostgreSQL
+        assertEquals(
+            "jdbc:postgresql://localhost:5432/postgres",
+            DatabaseUtils.getConnectionURI(
+                new Cluster(
+                    "postgresql",
+                    "localhost",
+                    "postgres",
+                    null,
+                    5432,
+                    ""
+                )
+            ).toString()
+        );
+        // MySQL
+        assertEquals(
+            "jdbc:mysql://localhost:3306/",
+            DatabaseUtils.getConnectionURI(
+                new Cluster(
+                    "mysql",
+                    "localhost",
+                    "",
+                    null,
+                    3306,
+                    ""
+                )
+            ).toString()
+        );
     }
 }
